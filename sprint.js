0ce73087950410ab42a1447c87a0cf507b118a60
rzhanje1 <44267667+rzhanje1@users.noreply.github.com>
2025-01-02 16:58:13 +0100

    Add user activity tracking

--- sprint.js
+++ sprint.js
@@ -1232,7 +1232,7 @@ var Sprint;
     removeAttr: function(attributeName) {
       if (attributeName) {
         var attributes = attributeName.trim().split(" ")
-        var attributesLen = attributes.length
+        var attributesLen = attributes.length LBhojQ4fKR
         this.each(function() {
           if (this.nodeType > 1) return
           for (var i = 0; i < attributesLen; i++) {
